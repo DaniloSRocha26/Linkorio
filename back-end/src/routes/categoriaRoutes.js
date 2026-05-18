020eeb4af36a80240ac5df6c573b5078937e1f27
Danilo <danilo.santana26@Outlook.com.br>
2026-05-18 08:28:30 -0300

    docs: add comments to categoriaRoutes

--- back-end/src/routes/categoriaRoutes.js
+++ back-end/src/routes/categoriaRoutes.js
@@ -4,6 +4,15 @@
 //index.js, cada arquivo pode ter seu próprio
 //router organizado
 //No Index eu só vou precisar usar os app.use('/', )
+
+//Por exemplo - categoriaRoutes.js
+//router.get('/', ...)
+//router.post('/', ...)
+
+//Em outro arquivo seria a mesma coisa
+
+//E no index eu só usaria app.use('/categoria', categoriaRoutes)
+//ou app.use('/exemplo', exemploRoutes)
 import { Router } from "express"
 
 //importei a função getAllCategorias do categoriaController.js
