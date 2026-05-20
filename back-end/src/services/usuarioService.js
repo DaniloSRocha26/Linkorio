6d8338982cf9765bc51bbc9a40494f9adcabe259
Danilo <danilo.santana26@Outlook.com.br>
2026-05-20 11:28:25 -0300

    feat: add login function and comments

--- back-end/src/services/usuarioService.js
+++ back-end/src/services/usuarioService.js
@@ -2,35 +2,48 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import db from "../config/db.js"
 
+//Função para regisrar o usuário pegando nome, email e senha
+//como parâmetros
 async function register(nome, email, senha){
-    const newSenha = await bcrypt.hash(senha, 10)
+    //bcrypt.hash está gerando um hash de senha, e 10 é o quanto o bcrypt
+    //trabalhar para gerar a senha
+    const hashSenha = await bcrypt.hash(senha, 10)
+
+    //Está inserindo o usuário no banco com a senha criptografada
     const resultado = await db.query(
         'INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3) RETURNING *',
-        [nome, email, newSenha]
+        [nome, email, hashSenha]
     );
+    //Está pegando o primeiro e unico resultado e retornando
     return resultado.rows[0];
 }
 
+//Função de Login do usuário
 async function login(email, senha){
     try{
-        const resultado = await db.query('SELECT * FROM usuarios WHERE email = $1', [email])
 
-        if (resultado.rows.length === 0){
-            throw new Error("Usuário não encontrado")
+        //Busca o usuário pelo email
+        const user = await db.query('SELECT * FROM usuarios WHERE email = $1', [email])
+
+        //Está verificando se nenhum usuário foi encontrado
+        if  (user.rows.length === 0){
+            throw new Error ("usuário não enconrado")
         }
+        const usuario = user.rows[0]
 
-        const usuario = resultado.rows[0]
-        const senhaCorreta = await bcrypt.compare(senha, usuario.senha)
+        //Compara a senha digitada com o hash salvo no banco de dados
+        const senhaComparada = await bcrypt.compare(senha, usuario.senha)
 
-        if (!senhaCorreta){
-            throw new Error("Senha incorreta")
+        if (senhaComparada === false){
+            throw new Error("Email ou senha incorretos")
         }
-
-        const token = jwt.sign({ id: usuario.id }, process.env.JWT_SECRET)
+        
+        //Gera um token com o idd do usuário dentro, assinado com a chave secreta
+        const token = jwt.sign({ id: usuario.id}, process.env.JWT_SECRET)
         return token
     } catch(error){
         throw error
     }
 }
 
-export { register, login }
+export {register, login}
