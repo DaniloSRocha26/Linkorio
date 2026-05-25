25521f3df9675d6ee0e9125ff5a0ae7c55cdb8e2
Danilo <danilo.santana26@Outlook.com.br>
2026-05-25 08:43:04 -0300

    feat: update categoriaController to filter categories by logged user

--- back-end/src/controllers/categoriaController.js
+++ back-end/src/controllers/categoriaController.js
@@ -1,11 +1,13 @@
 //Importo a função getAll do categoria.Service.js
-import getAll from "../services/categoriaService.js";
+import { getAll } from "../services/categoriaService.js";
 
 //Faço uma função assíncrona para pegar todas as categorias e devolver
 //como um json
 async function getAllCategorias(req, res){
     try{
-       const categorias = await getAll()
+        //Passo o id do usuário logado que o middleware autenticar
+        //salvou em req.user após validar o token
+       const categorias = await getAll(req.user.id)
        res.json(categorias)
     }catch(error){
         res.status(500).json({ message: error.message})
